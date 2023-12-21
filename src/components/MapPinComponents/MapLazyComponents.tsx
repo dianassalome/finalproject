7a6472aae49f634cec5e6ba8fe366981e16f024f
Diana <diana.snf@gmail.com>
2023-12-21 11:31:55 +0000

    fixes

--- src/components/MapPinComponents/MapLazyComponents.tsx
+++
@@ -1,7 +0,0 @@
-import { MapContainer as LMapContainer } from "react-leaflet";
-
-
-//any
-export const MapContainer = ({ forwardedRef, ...props }: any) => (
-  <LMapContainer {...props} ref={forwardedRef} />
-);
